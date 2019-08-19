835720ac995577b47c5bf835c85fe28e5ac91e1e
Finlay Cannon <fin@finlaycannon.com>
2019-08-18 22:05:30 -0700

    vars come second

--- src/components/time.js
+++ src/components/time.js
@@ -1,12 +1,12 @@
 const right = (len, str) => str.substr(-len);
 
-const pad = (number, len = 2) => {
+const pad = (len, number) => {
     const paddedDigits = '0'.repeat(len) + number;
     return right(len, paddedDigits);
 };
 
 export const parseDigits = digits => {
-    const paddedDigits = pad(digits, 5);
+    const paddedDigits = pad(5, digits);
     return {
         hours: paddedDigits.substr(0, 1),
         mins: paddedDigits.substr(1, 2),
@@ -20,8 +20,8 @@ export const parseTime = millis => {
     const mins = Math.floor(seconds / 60) - hours * 60;
     const secs = seconds - hours * 3600 - mins * 60;
     return {
-        hours: pad(hours, 1),
-        mins: pad(mins),
-        secs: pad(secs),
+        hours: pad(1, hours),
+        mins: pad(2, mins),
+        secs: pad(2, secs),
     };
 };
